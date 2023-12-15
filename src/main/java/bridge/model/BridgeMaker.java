e7748a69c2cdef353b5c529e9aea727a96f83933
alswp006 <alswp006@gmail.com>
2023-12-15 12:07:26 +0900

    1차 구현 완료(수정)

--- src/main/java/bridge/model/BridgeMaker.java
+++ src/main/java/bridge/model/BridgeMaker.java
@@ -4,13 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BridgeMaker {
-
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
-
 
     public List<String> makeBridge(int size) {
         List<String> numbers = new ArrayList<>();
--- src/main/java/bridge/model/BridgeRandomNumberGenerator.java
+++ src/main/java/bridge/model/BridgeRandomNumberGenerator.java
@@ -4,7 +4,6 @@ import bridge.model.BridgeNumberGenerator;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class BridgeRandomNumberGenerator implements BridgeNumberGenerator {
-
     private static final int RANDOM_LOWER_INCLUSIVE = 0;
     private static final int RANDOM_UPPER_INCLUSIVE = 1;
 
